bf47809c7d90996ddf6eec1a361da4a7c4d4aab8
Kris Scott <kscott91@gmail.com>
2023-05-13 11:43:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ai-imagen
+
+Create new images based on the given filenames.
+
+A minimal usage example of [HuggingFace Transformers Agent](https://huggingface.co/docs/transformers/transformers_agents).
+
+## Usage
+
+```
+python imagen.py filename1 [filename2 ...]
+```
+
+## Example
+
+```
+python imagen.py corgi-in-space.png
+```
+
+Output:
+
+![corgi-in-space.png](docs/corgi-in-space.png)
---
+++ imagen.py
@@ -0,0 +1,49 @@
+# pyright: reportUnknownMemberType=false
+# pyright: reportUnknownVariableType=false
+# pyright: reportUnknownArgumentType = false
+# pyright: reportMissingTypeStubs=false
+
+from typing import Any
+from dotenv import load_dotenv
+from transformers import OpenAiAgent
+import os
+import sys
+
+
+def anytype(x: Any) -> Any:
+    return x
+
+
+def slugify(s: str) -> str:
+    return "".join(x for x in s.replace(" ", "-") if x.isalnum() or x == "-")
+
+
+def main():
+    _ = load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    assert api_key
+
+    agent = OpenAiAgent(model="text-davinci-003", api_key=api_key)
+
+    for filepath in sys.argv[1:]:
+        # Assert file does not exist
+        assert not os.path.exists(filepath)
+
+        filename = os.path.split(filepath)[-1]
+        print("Generating", filename)
+
+        # Assert file extension is valid
+        assert os.path.splitext(filename)[-1].lower() in [".jpg", ".jpeg", ".png"]
+
+        # Generate an image
+        image: Any = agent.run(
+            "Generate an image `filename`",
+            remote=True,
+            filename=filename,
+        )
+
+        image.save(filepath)
+
+
+if __name__ == "__main__":
+    main()
